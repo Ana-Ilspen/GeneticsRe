c9e24e2991635d56ef09ae87509af11ed43c8e4a
Ana-Ilspen <ilspenanastasia@gmail.com>
2026-02-15 00:15:01 +0530

    Refactor root element initialization in index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+
 root.render(
   <DndProvider backend={HTML5Backend}>
     <App />
